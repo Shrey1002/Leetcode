ac4bff788e65d6d1f90b0949a12ca71173426978
Shrey Agrawal <68065864+Shrey1002@users.noreply.github.com>
2025-03-03 18:17:00 -0500

    Time: 2664 ms (60.07%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,9 +1,4 @@
--- Write your PostgreSQL query statement below
-select 
-CASE 
-        WHEN eu.unique_id IS NULL THEN NULL 
-        ELSE eu.unique_id 
-    END AS unique_id,
-    e.name
-from Employees as e left outer join EmployeeUNI as eu
-on e.id = eu.id
+/* Write your T-SQL query statement below */
+select u.unique_id, e.name  from Employees as e left outer join EmployeeUNI as u on e.id = u.id 
+
+-- u.unique_id, e.name
