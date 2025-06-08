caaa3b25430bf6e9ea7e73adecf8cfca12cf1d6c
Shrey Agrawal <68065864+Shrey1002@users.noreply.github.com>
2025-06-08 12:01:38 -0400

    Time: 12 ms (90.15%) | Memory: 12.5 MB (7.10%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -7,22 +7,40 @@ class Solution(object):
         # print(x.bit_length())
         # if x >= (2**31)
         # print(x.bit_length())
-        x = str(x)
-        if x[0] == '-':
-            x = x[1:]
-            x = x[::-1]
-            x = int(x)
-            if x >= -(2**31) and x < (2**31):
-                return x*-1
-            else:
-                return 0
+        # x = str(x)
+        # if x[0] == '-':
+        #     x = x[1:]
+        #     x = x[::-1]
+        #     x = int(x)
+        #     if x >= -(2**31) and x < (2**31):
+        #         return x*-1
+        #     else:
+        #         return 0
+        # else:
+        #     x = x[::-1]
+        #     x = int(x)
+        #     if x >= -(2**31) and x < (2**31):
+        #         return x
+        #     else:
+        #         return 0
+        min_int = (2**31)*-1
+        max_int = (2**31) -1
+        negative = x<0
+        x = abs(x)
+        reverse_num = 0
+
+        while(x>0):
+            num = x % 10
+            x = x//10
+            reverse_num = (reverse_num*10) + num
+        # return reverse_num*-1
+        if reverse_num < min_int or reverse_num > max_int:
+            return 0
+        elif negative:
+            return reverse_num*-1
         else:
-            x = x[::-1]
-            x = int(x)
-            if x >= -(2**31) and x < (2**31):
-                return x
-            else:
-                return 0
+            return reverse_num
+
 
 
         # print(ans)
